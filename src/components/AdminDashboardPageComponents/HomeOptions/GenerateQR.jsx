ca72e3f6284c92cd642049bfc6ba0c8a9957c4d5
Febin-Joseph <febin.joseph@checkmed.in>
2024-06-21 21:58:22 +0530

    redirect

--- src/components/AdminDashboardPageComponents/HomeOptions/GenerateQR.jsx
+++ src/components/AdminDashboardPageComponents/HomeOptions/GenerateQR.jsx
@@ -121,7 +121,8 @@ const GenerateQR = ({
   };
 
   const handleNavigate = () => {
-    window.open(`/home-page/${city}/${companyName}`);
+    navigate(`/home-page/${city}/${companyName}`);
+    window.location.href = `/home-page/${city}/${companyName}`
   };
 
   const qrCodeValue = `/home-page/${city}/${companyName}`;
